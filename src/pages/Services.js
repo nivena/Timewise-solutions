662ef1a5567704ba7b1aaf35276ba2324aae8c4f
Niven Aldridge <niv.aldridge@gmail.com>
2023-08-26 18:14:36 +1000

    services

---
+++ src/pages/Services.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Services - Timewise Solutions</title>
+  <link rel="stylesheet" href="../styles/main.css">
+</head>
+<body>
+
+  <!-- Header -->
+  <header>
+    <h1>Our Services</h1>
+  </header>
+
+  <!-- Navigation -->
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="services.html">Services</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="order-online.html">Order Online</a></li>
+    </ul>
+  </nav>
+
+  <!-- Main Content -->
+  <main>
+    <section id="blockchain-solutions">
+      <h2>Blockchain Solutions</h2>
+      <p>We offer cutting-edge blockchain solutions for various industries.</p>
+    </section>
+
+    <section id="accounting-solutions">
+      <h2>Accounting Solutions</h2>
+      <p>Our accounting solutions are designed to streamline your financial processes.</p>
+    </section>
+
+    <section id="business-solutions">
+      <h2>Business Solutions</h2>
+      <p>We provide comprehensive business solutions to help you achieve your goals.</p>
+    </section>
+  </main>
+
+  <!-- Footer -->
+  <footer>
+    <p>Copyright © 2023 Timewise Solutions</p>
+  </footer>
+
+  <script src="../scripts/main.js"></script>
+</body>
+</html>
