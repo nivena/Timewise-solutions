74b5c33d64a2440a19365aa2f61b37cdec384e94
Niven Aldridge <niv.aldridge@gmail.com>
2023-08-26 18:09:28 +1000

    Initial Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Home - My Website</title>
+  <link rel="stylesheet" href="../styles/main.css">
+</head>
+<body>
+
+  <!-- Header -->
+  <header>
+    <h1>Welcome to My Website</h1>
+  </header>
+
+  <!-- Navigation -->
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="services.html">Services</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="order-online.html">Order Online</a></li>
+    </ul>
+  </nav>
+
+  <!-- Main Content -->
+  <main>
+    <h2>About Us</h2>
+    <p>We are a company that specializes in providing excellent services.</p>
+  </main>
+
+  <!-- Footer -->
+  <footer>
+    <p>Copyright © 2023 My Website</p>
+  </footer>
+
+  <script src="../scripts/main.js"></script>
+</body>
+</html>
